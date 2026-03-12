720e3cf3a77cdfc464281d19f700003d31526942
sa-buc <wumingdong.wmd@z59l09406.na61>
2026-03-12 11:28:33 +0800

    print something

--- nanovllm/engine/model_runner.py
+++ nanovllm/engine/model_runner.py
@@ -207,6 +207,11 @@ class ModelRunner:
 
     def run(self, seqs: list[Sequence], is_prefill: bool) -> list[int]:
         input_ids, positions = self.prepare_prefill(seqs) if is_prefill else self.prepare_decode(seqs)
+        print("prepare_prefill" if is_prefill else "prepare_decode")
+        print(input_ids.shape, positions.shape)
+        print(input_ids)
+        print(positions)
+        print("--------------------------------")
         temperatures = self.prepare_sample(seqs) if self.rank == 0 else None
         logits = self.run_model(input_ids, positions, is_prefill)
         token_ids = self.sampler(logits, temperatures).tolist() if self.rank == 0 else None
